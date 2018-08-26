d868ff78e28fadba71d700538725b3d7136bac95
Zuzu <mikelzuzu@gmail.com>
2018-08-26 17:23:32 +0100

    refactoring to common crud repository

--- pet-clinic-data/src/main/java/com/mikelzuzu/dipetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/mikelzuzu/dipetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.mikelzuzu.dipetclinic.services;
 
 import com.mikelzuzu.dipetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet Pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/mikelzuzu/dipetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/mikelzuzu/dipetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.mikelzuzu.dipetclinic.services;
 
 import com.mikelzuzu.dipetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet Vet);
-
-    Set<Vet> findAll();
 }
